10ed9ac5f2151f15af94ebd85e96f58c7c808438
angelis-reis <angelisreisbiof@gmail.com>
2021-03-02 03:22:41 -0300

    faltando pop-up do trailer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Row from './Row'
+
+import Banner from "./Banner" 
+
+import Nav from "./Nav"
+
+import requests from "./requests"
+
+import "./App.css"
+
+
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="App" >
+      <Nav />
+      <Banner />
+      <Row isLargeRow= {true} title= "NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} />
+      <Row title= "Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title= "Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title= "Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title= "Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title= "Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title= "Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title= "Documentaries" fetchURL={requests.fetchDocumentaires} />
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,22 @@
+const API_KEY = "3f6079589af38653e6645a25c5de65ea";
+
+const requests = {
+    
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+
+    
+    fetchNetflixOriginals:`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+
+    
+    fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=pt-BR&page=1`,
+
+    
+
+    fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaires: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
